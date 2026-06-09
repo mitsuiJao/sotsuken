33a8daf95865d967e3f15bfefd2558dc8e1a337c
mitsuijao <nishima.kun@gmail.com>
2026-06-09 10:12:55 +0000

    rsi

---
+++ src/get_stockprices/rsi.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import numpy as np
+
+# Open  High  Low  Close  Volume  Dividends  Stock Splits
+
+N = 14
+
+df = pd.read_parquet("./AAPL.parquet")
+df["diff"] = df["Close"].diff(1)
+print(df)
+
+stock = df[["Open", "High", "Low", "Close", "Volume"]].tail(60)
+
+df["gain"] = np.where(df["diff"] > 0, df["diff"], 0)
+df["loss"] = np.where(df["diff"] < 0, -df["diff"], 0)
+# emw
+ave_gain = df["gain"].ewm(alpha=1/N, adjust=False).mean()
+ave_loss = df["loss"].ewm(alpha=1/N, adjust=False).mean()
+rs = ave_gain / ave_loss
+rsi = 100 - (100 / (1 + rs))
+
+rsi_trimmed = rsi.loc[stock.index]
+
+ap = mpf.make_addplot(rsi_trimmed, panel=1, color="blue", ylabel="RSI")
+mpf.plot(stock, type="candle", style="yahoo", volume=False, addplot=ap, savefig="./aapl_chart.png")
